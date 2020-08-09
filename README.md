99ad74a81b6793c565f3fe82a534f3de80b64ed3
pinglu85 <luping8576@hotmail.com>
2020-08-09 16:49:46 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Vanilla JavaScript Text Animation Tutorial - Franks laboratory
---
+++ script.js
@@ -0,0 +1,121 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const particles = [];
+const adjustX = 2;
+const adjustY = -5;
+
+// handle mouse
+const mouse = {
+  x: null,
+  y: null,
+  radius: 250,
+};
+
+window.addEventListener('mousemove', function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
+
+ctx.fillStyle = 'white';
+ctx.font = '30px Verdana';
+ctx.fillText('YEAH', 0, 30);
+// Scan the underlying pixel data of the text area
+const textCoordinates = ctx.getImageData(0, 0, 100, 100);
+
+class Particle {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = 3;
+    this.baseX = this.x;
+    this.baseY = this.y;
+    this.density = Math.random() * 40 + 3;
+    this.color = 'white';
+  }
+
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+  }
+  update() {
+    const dx = mouse.x - this.x;
+    const dy = mouse.y - this.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    const forceDirectionX = dx / distance;
+    const forceDirectionY = dy / distance;
+    const maxDistance = mouse.radius;
+    const force = (maxDistance - distance) / maxDistance;
+    const directionX = forceDirectionX * force * this.density;
+    const directionY = forceDirectionY * force * this.density;
+
+    if (distance < mouse.radius) {
+      this.color = 'yellow';
+      this.x -= directionX;
+      this.y -= directionY;
+    } else {
+      if (this.x !== this.baseX) {
+        const dx = this.x - this.baseX;
+        this.x -= dx / 10;
+      }
+      if (this.y !== this.baseY) {
+        const dy = this.y - this.baseY;
+        this.y -= dy / 10;
+      }
+      this.color = 'white';
+    }
+  }
+}
+
+function init() {
+  for (let y = 0, y2 = textCoordinates.height; y < y2; y++) {
+    for (let x = 0, x2 = textCoordinates.width; x < x2; x++) {
+      // Check pixel opacity is more than 50%
+      if (
+        textCoordinates.data[y * 4 * textCoordinates.width + x * 4 + 3] > 128
+      ) {
+        const positionX = (x + adjustX) * 15;
+        const positionY = (y + adjustY) * 15;
+        particles.push(new Particle(positionX, positionY));
+      }
+    }
+  }
+}
+init();
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].draw();
+    particles[i].update();
+  }
+  connect();
+  requestAnimationFrame(animate);
+}
+animate();
+
+function connect() {
+  let opacity = 1;
+  for (let a = 0; a < particles.length; a++) {
+    for (let b = a; b < particles.length; b++) {
+      const dx = particles[a].x - particles[b].x;
+      const dy = particles[a].y - particles[b].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < 40) {
+        opacity = 1 - distance / 50;
+        ctx.strokeStyle = particles[b].color;
+        ctx.globalAlpha = opacity;
+        ctx.lineWidth = 2;
+        ctx.beginPath();
+        ctx.moveTo(particles[a].x, particles[a].y);
+        ctx.lineTo(particles[b].x, particles[b].y);
+        ctx.stroke();
+      }
+    }
+  }
+}
